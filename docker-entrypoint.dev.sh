63c481d6f5e1401eb94c7c367ed3ef8351895461
Doug Emery <emeryr@upenn.edu>
2026-08-17 16:42:49 -0400

    Force development RAILS_ENV in docker-entrypoint.dev.sh

--- docker-entrypoint.dev.sh
+++ docker-entrypoint.dev.sh
@@ -6,7 +6,7 @@ if [ -f /app/tmp/pids/server.pid ]; then
   rm /app/tmp/pids/server.pid
 fi
 
-bundle exec rake db:migrate:with_data 2>/dev/null || bundle exec rake db:setup
+RAILS_ENV=development bundle exec rake db:migrate:with_data || RAILS_ENV=development bundle exec rake db:setup
 RAILS_ENV=test bundle exec rails db:prepare
 
 exec bundle exec "$@"
